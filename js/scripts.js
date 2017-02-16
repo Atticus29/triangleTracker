aa867c03ea6f59eb0701d2582cab13999c16696e
Alex Lee and Mark Fisher <mark@>
2017-02-16 15:51:31 -0800

    made if branching a function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,53 @@
+var assessSides = function(side1, side2, side3){
+  if((side1 + side2 <= side3) || (side2 + side3 <= side1) || (side1 + side3 <= side2)){
+    var returnVal = "not a triangle");
+  } else{
+    if(side1 === side2 && side1 === side3 && side2 === side3){
+      var returnVal = "equilateral");
+    } else if (side1 != side2 && side1!= side3 && side2 != side3){
+      var returnVal = "scalene")
+    } else if (side1 === side2 || side1 === side3 || side2 === side3){
+      var returnVal = "isosceles");
+    }
+  }
+  return returnVal;
+}
+
+var assessSidesV2 = function(side1, side2, side3){
+  //side1 and side2
+  if (side1===side2){
+    if(side3 >= side1+side2){
+      var returnVal = "not a traingle");
+    }else if(side1===side3){
+      var returnVal = "equilateral");
+    }else{
+      var returnVal = "isoceles");
+    }
+  //side2 and side3
+  }else if(side2===side3){
+    if (side1 >= side2+side3){
+      var returnVal = "not a traingle");
+    }else if(side2===side1){
+      var returnVal = "equilateral");
+    }else{
+      var returnVal = "isoceles");
+    }
+  //side1 and side3
+  }else if (side1===side3){
+    if (side2 >= side1+side3){
+      var returnVal = "not a triangle");
+    }else if(side1 ===side2){
+      var returnVal = "equilateral");
+    }else {
+      var returnVal = "Isoceles");
+    }
+  }else{
+    var returnVal = "Scalene");
+  }
+  return returnVal;
+}
+
+
 $(function(){
   $("#triangleForm").submit(function(){
     // console.log("Got in");
@@ -6,48 +56,9 @@ $(function(){
     var side3 = parseInt($("#side3").val());
     // debugger;
     console.log(side1);
-    if((side1 + side2 <= side3) || (side2 + side3 <= side1) || (side1 + side3 <= side2)){
-      console.log("not a triangle");
-    } else{
-      if(side1 === side2 && side1 === side3 && side2 === side3){
-        console.log("equilateral");
-      } else if (side1 != side2 && side1!= side3 && side2 != side3){
-        console.log("scalene")
-      } else if (side1 === side2 || side1 === side3 || side2 === side3){
-        console.log("isosceles");
-      }
-    }
 
-    // //side1 and side2
-    // if (side1===side2){
-    //   if(side3 >= side1+side2){
-    //     console.log("not a traingle");
-    //   }else if(side1===side3){
-    //     console.log("equilateral");
-    //   }else{
-    //     console.log("isoceles");
-    //   }
-    // //side2 and side3
-    // }else if(side2===side3){
-    //   if (side1 >= side2+side3){
-    //     console.log("not a traingle");
-    //   }else if(side2===side1){
-    //     console.log("equilateral");
-    //   }else{
-    //     console.log("isoceles");
-    //   }
-    // //side1 and side3
-    // }else if (side1===side3){
-    //   if (side2 >= side1+side3){
-    //     console.log("not a triangle");
-    //   }else if(side1 ===side2){
-    //     console.log("equilateral");
-    //   }else {
-    //     console.log("Isoceles");
-    //   }
-    // }else{
-    //   console.log("Scalene");
-    // }
+
+
 
     event.preventDefault();
   });
